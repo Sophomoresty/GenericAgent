26215796fbdf2d1f170c634cdaa1481f12e20dbd
Sophomores <1404462714@qq.com>
2026-04-21 18:01:41 +0800

    fix: hide ga gui launcher window

--- ga.bat
+++ ga.bat
@@ -18,5 +18,15 @@ goto :eof
 
 :gui
 shift
-start "" .venv\Scripts\pythonw.exe ga-gui.pyw %1 %2 %3 %4 %5 %6 %7 %8 %9
+set "GA_GUI_ARGS=ga-gui.pyw"
+if not "%~1"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~1"
+if not "%~2"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~2"
+if not "%~3"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~3"
+if not "%~4"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~4"
+if not "%~5"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~5"
+if not "%~6"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~6"
+if not "%~7"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~7"
+if not "%~8"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~8"
+if not "%~9"=="" set "GA_GUI_ARGS=%GA_GUI_ARGS% %~9"
+powershell -NoProfile -WindowStyle Hidden -Command "Start-Process -WindowStyle Hidden -FilePath '.venv\Scripts\pythonw.exe' -ArgumentList '%GA_GUI_ARGS%' -WorkingDirectory 'D:\code\GenericAgent'"
 endlocal
